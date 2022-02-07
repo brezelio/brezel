a73de40ec2d6fe6dfb9b19be857169f4abb39e97
Oliver.Wich <oliverwichmann@outlook.de>
2022-02-07 16:13:51 +0100

    lint fix

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <BrezelApp>
     <template slot="logo">
-      <img src="/assets/logo.svg" style="width: 100%; max-height: 100px" alt="Logo">
+      <img
+        src="/assets/logo.svg"
+        style="width: 100%; max-height: 100px"
+        alt="Logo"
+      >
     </template>
     <template slot="loginLogo">
-      <img src="/assets/logo.svg" :style="{ maxWidth: '100%', margin: '0 auto 26px auto', display: 'block' }" alt="logo">
+      <img
+        src="/assets/logo.svg"
+        :style="{ maxWidth: '100%', margin: '0 auto 26px auto', display: 'block' }"
+        alt="logo"
+      >
     </template>
   </BrezelApp>
 </template>
-
 <script>
 import BrezelApp from '@kibro/brezel-spa/src/components/App'
 
