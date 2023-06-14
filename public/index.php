2f9876469d2b766b8b80b9e60fecd9b50b2c607c
OliverWich <oliverwichmann@outlook.de>
2023-06-14 11:50:20 +0200

    fix: use correct base path

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use App\Brezel;
 
 $brezel = new Brezel();
-$brezel->setBasePath(realpath('..'));
+$brezel->setBasePath(realpath(__DIR__ . '/..'));
 $brezel->handle();
