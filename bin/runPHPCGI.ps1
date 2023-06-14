5b01524ef2848167c48c62caf09ddc27486c2fd4
OliverWich <oliverwichmann@outlook.de>
2023-06-14 17:46:50 +0200

    fix: cgi ini path on windows

--- bin/runPHPCGI.ps1
+++ bin/runPHPCGI.ps1
@@ -2,6 +2,8 @@
 $env:PHP_FCGI_MAX_REQUESTS = 0
 $env:PHP_FCGI_CHILDREN = 5
 
+$iniFile = ((php -i | find /i "Loaded Configuration File") -replace "Loaded Configuration File =>", "").Trim()
+
 # Start fastcgi
-Write-Host 'Starting php-cgi on 127.0.0.1:8099 with 5 children' -ForegroundColor DarkGreen
-php-cgi -b 127.0.0.1:8099
+Write-Host "Starting php-cgi on 127.0.0.1:8099 with 5 children and ini file at $iniFile" -ForegroundColor DarkGreen
+php-cgi --php-ini $iniFile -b 127.0.0.1:8099
