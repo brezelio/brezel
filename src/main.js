dad0f0277aaf7038e416c99b5b11218e3450061c
Tim Nissen <tim.nissen@kiwis-and-brownies.de>
2023-06-29 10:16:34 +0000

    fix: added translation for recipes

--- src/main.js
+++ src/main.js
@@ -8,5 +8,6 @@ const brezel = new Brezel(import.meta.env.VITE_APP_API_URL, import.meta.env.VITE
 const app = brezel.bootstrap(App)
 
 window.app = app
+window.app.$t = app.__VUE_I18N__.global.t
 
 export default app.mount('#app')
