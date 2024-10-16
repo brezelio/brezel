a1d1b3d7e281d57c0b1a359001a9b60d54d4a43c
muneer <muneer.ahamed@kiwis-and-brownies.de>
2024-10-16 12:33:16 +0530

    Added workflow, modals and modified offer module to Manually confirm Offer and create a  new project

---
+++ src/components/ManuallyConfirmOffer.vue
@@ -0,0 +1,68 @@
+{% verbatim %}
+<template>
+   <div class="main-div">
+      <h3>display PDF here..</h3>
+      <div class="header">
+         <div class="header-left">
+            <div style="font-size: 8pt;">
+               BWA Sauerland GmbH Peter-Dassis-Ring 40a 57482 Wenden<br>
+               02762-98 19 499 info@bwa-sauerland.de
+            </div>
+         </div>
+         <div class="header-right">
+            <div>
+               <table class="noborder" style="font-size: 10pt;width: 100%">
+                  <tr>
+                     <td>
+                        Rechnungsdatum
+                     </td>
+                     <td class="number-td"><b>He</b></td>
+                  </tr>
+
+                  <tr>
+                     <td>Projektnummer</td>
+                     <td class="number-td"><b>de</b></td>
+                  </tr>
+
+               </table>
+            </div>
+         </div>
+      </div>
+      <div class="customer-info" style="line-height:1;">
+         <p>customer name {{offers}}</p>
+         <p>customer address</p>
+      </div>
+   </div>
+</template>
+
+<script>
+import { Api } from '@kibro/brezel-spa'
+// import Api from 'api'
+// import { apiGet } from 'apiFunctions'
+
+export default {
+   data() {
+      return {
+
+      }
+   },
+   methods: {
+      getOffers() {
+        
+         return Api.getModule('offers')
+
+      }
+
+   }
+}
+// ------------------------------------------//
+
+
+</script>
+<style>
+.header {
+   display: flex;
+   justify-content: space-between;
+}
+</style>
+{% endverbatim %}
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 // Local Dist / Build
 import { Brezel } from '@kibro/brezel-spa'
 import CreateOrder from './components/CreateOrder.vue'
+import ManuallyConfirmOffer from './components/ManuallyConfirmOffer.vue'
 
 // Linked
 // import Brezel from '@kibro/brezel-spa/src/brezel'
@@ -11,8 +12,8 @@ import CreateOrder from './components/CreateOrder.vue'
 const brezel = new Brezel(import.meta.env.VITE_APP_API_URL, import.meta.env.VITE_APP_SYSTEM)
 const app = brezel.bootstrap(App)
 
-app.component('CreateOrder', CreateOrder)
-
+app.component('CreateOrder', CreateOrder);
+app.component('ManuallyConfirmOffer',ManuallyConfirmOffer);
 window.app = app
 
 export default app.mount('#app')
