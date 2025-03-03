6473be02468745c0d5d949c0a97e1da55d160308
OliverWich <kontakt@oliver-wich.de>
2025-03-03 15:14:43 +0100

    chore: cleanup structure

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import '@kibro/brezel-spa/dist-lib/style.css'
 import App from './App.vue'
 
-// Local Dist / Build
 import { Brezel } from '@kibro/brezel-spa'
-
-// Linked
-// import Brezel from '@kibro/brezel-spa/src/brezel'
 
 const brezel = new Brezel(import.meta.env.VITE_APP_API_URL, import.meta.env.VITE_APP_SYSTEM)
 const app = brezel.bootstrap(App)
