cef03bf16cf3c61c1916ea151cdba4c89cb0b6b1
OliverWich <kontakt@oliver-wich.de>
2025-07-16 13:58:44 +0200

    fix: use vendored eslint and prettier

---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+import brezelSpaEslintConfig from '@kibro/brezel-spa/eslint-config'
+
+export default [
+  ...brezelSpaEslintConfig,
+  {
+    // Your project-specific rules or overrides
+  }
+]
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+import brezelSpaPrettierConfig from '@kibro/brezel-spa/prettier-config';
+
+export default {
+  ...brezelSpaPrettierConfig,
+  // Any project-specific overrides can go here
+};
