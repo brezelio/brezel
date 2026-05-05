68d21a36f2668a1e22cc042cb750b1669af8caa3
OliverWich <kontakt@oliver-wich.de>
2026-05-05 15:45:33 +0200

    fix: make brezel teardown remove pma too

--- tooling/brezel/lib/compose.ts
+++ tooling/brezel/lib/compose.ts
@@ -1,10 +1,12 @@
 import { runProjectCommand, runProjectCommandCaptured, runProjectCommandInteractive, runProjectCommandStreamingCaptured, type CapturedCommandResult, type StreamingCaptureOptions } from "./exec"
 
+const optionalProfiles = ["db-explore"]
+
 export function runComposeCommand(args: string[]): number {
   return runProjectCommand({
     unixCommand: "bin/compose",
     windowsCommand: "bin\\compose.bat",
-    args,
+    args: withOptionalProfiles(args),
   })
 }
 
@@ -12,7 +14,7 @@ export async function runComposeCommandInteractive(args: string[]): Promise<numb
   return await runProjectCommandInteractive({
     unixCommand: "bin/compose",
     windowsCommand: "bin\\compose.bat",
-    args,
+    args: withOptionalProfiles(args),
   })
 }
 
@@ -20,7 +22,7 @@ export function runComposeCommandCaptured(args: string[]): CapturedCommandResult
   return runProjectCommandCaptured({
     unixCommand: "bin/compose",
     windowsCommand: "bin\\compose.bat",
-    args,
+    args: withOptionalProfiles(args),
   })
 }
 
@@ -28,6 +30,10 @@ export async function runComposeCommandStreamingCaptured(args: string[], options
   return await runProjectCommandStreamingCaptured({
     unixCommand: "bin/compose",
     windowsCommand: "bin\\compose.bat",
-    args,
+    args: withOptionalProfiles(args),
   }, options)
+}
+
+function withOptionalProfiles(args: string[]): string[] {
+  return [...optionalProfiles.flatMap((profile) => ["--profile", profile]), ...args]
 }
